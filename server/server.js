91145997ff635ef8996be284eae0614fda0175c8
hibow <utfnalin@gmail.com>
2019-06-03 17:27:30 -0500

    add svg and css

--- server/server.js
+++ server/server.js
@@ -4,7 +4,8 @@ const path = require('path');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
-
+app.use(morgan('dev'));
+app.use(cors());
 // app.use(morgan('dev'));
 // app.use(express.static(path.join(__dirname, 'public')));
 // app.get('/', function(req, res) {
@@ -13,8 +14,7 @@ const port = process.env.PORT || 3000;
 // app.use('/:id', express.static(`${__dirname}/../public`));
 app.use('/bizs/:bId', express.static(`${__dirname}/../public`));
 app.use(express.static(`${__dirname}/../public`));
-app.use(morgan('dev'));
-app.use(cors());
+
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
 });
